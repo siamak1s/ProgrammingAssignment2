126d20dc5c444272af39db1a03f0980019a34692
siamak1s <talebisiamak@gmail.com>
2016-02-26 22:43:44 -0500

    Updated assignement2

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,22 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { ##this function is making squar matrix
 
+		m <- NULL       #first m will be Null
+        set <- function(y) { 
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() # for getting the x matrix
+		x
+        setinversion <- function(solve)
+		m <<- solve
+        getinversion <- function() 
+		m
+        list(set = set, get = get,   #list which consist of functions
+             setinversion = setinversion ,
+             getinversion = getinversion)
 }
 
 
@@ -12,4 +26,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+		m <- x$getinversion()
+        if(!is.null(m)) {       #it controls to see if the m is empty or not
+                message("getting cached data") #if it is not empty the inverse will be get from m
+                return(m) #m will be returned
+        }
+        data <- x$get()  #if the m is empty the inverse will be calculated and will be put in m
+        m <- solve(data, ...)
+        x$setinversion(m)
+        m
 }
